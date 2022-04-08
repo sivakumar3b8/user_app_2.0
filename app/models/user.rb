6574583d61fefe9e3946a2aed8b474dae22c5dea
sivakumar <sivakumar3b8@gmail.com>
2022-04-08 15:27:08 +0530

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+
 end
